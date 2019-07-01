a9e5a776f2325b1e866d5be0c78fe4c587165c32
LCF.ERR <50033588+tfqhkm@users.noreply.github.com>
2019-07-01 14:13:01 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+# WOI KIMAK TAKKAN TOOLS SENANG PON NAK RECODE BABIHHH BAPAK NOOB GILER
+clear
+python2 NOTHIN2.py
+clear
+python2 NOTHIN.py
+pkg update
+pkg upgrade
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+clear
+figlet "WAIT" | lolcat
+sleep 5
+pkg install metasploit
+figlet "DONE" | lolcat
+
